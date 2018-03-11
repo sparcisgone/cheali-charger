cb2e234e8d6e7c0984ac536296ca17040d60915d
sparcisgone <37261143+sparcisgone@users.noreply.github.com>
2018-03-11 13:20:26 +0100

    MinGW target

--- avr-toolchain.cmake
+++ avr-toolchain.cmake
@@ -1,5 +1,5 @@
 
 SET(CMAKE_SYSTEM_NAME Generic)
 
-SET(CMAKE_C_COMPILER avr-gcc)
-SET(CMAKE_CXX_COMPILER avr-g++)
+SET(CMAKE_C_COMPILER C:/Gnu/MHVAVR/bin/avr-gcc.exe)
+SET(CMAKE_CXX_COMPILER C:/Gnu/MHVAVR/bin/avr-g++.exe)
